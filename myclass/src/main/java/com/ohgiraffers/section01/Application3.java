507f83f9abea5f6de00396edb76a40edbf591ac8
weed97 <weed9935@gmail.com>
2024-06-17 18:43:30 +0900

    Application3

--- myclass/src/main/java/com/ohgiraffers/section01/Application3.java
+++ myclass/src/main/java/com/ohgiraffers/section01/Application3.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
+import static com.ohgiraffers.common.JDBCTemplate.close;
 import static com.ohgiraffers.common.JDBCTemplate.getConnection;
 
 public class Application3 {
@@ -17,20 +18,32 @@ public class Application3 {
         Statement stmt = null;
 
         // 3. ResultSet 생성 ->
-        ResultSet rest = null;
+        ResultSet rset = null;
 
-//        try {
-//            // 4. 연결객체의 createStatement()를 이용한 Statement 객체 생성
-//            try {
-//                stmt = con.createStatement();
-//                Scanner sc = new Scanner(System.in);
-//                System.out.println("조회하려는 사번을입력하세요 : ");
-//                String query = "select emp_id,emp_name from employee where subjectNo = '" + subjectNo + "'";
-//            } catch (SQLException e) {
-//                throw new RuntimeException(e);
-//            }
-//
-//        }
+
+            // 4. 연결객체의 createStatement()를 이용한 Statement 객체 생성
+            try {
+                stmt = con.createStatement();
+                Scanner sc = new Scanner(System.in);
+                System.out.println("조회하려는 사번을입력하세요 : ");
+                String subjectNo = sc.nextLine();
+                String query = "select subject_no,subject_name from favorite_subject where subjectNo = '" + subjectNo + "'";
+                // 5. executeQuery()로 쿼리문을 실행하고 결과를 ResultSet 반환 받기
+                rset = stmt.executeQuery(query);
+                
+                // 6. 쿼리문의 결과를 컬럼 이름을 이용해서 사용
+                if (rset.next()) {
+                    System.out.println(rset.getString("SUBJECT_ID") + ',' + rset.getString("SUBJECT_NAME"));
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } finally {
+                // 7. 사용한 자원 반납
+                close(rset);
+                close(stmt);
+                close(con);
+            }
+
 
     }
 }
