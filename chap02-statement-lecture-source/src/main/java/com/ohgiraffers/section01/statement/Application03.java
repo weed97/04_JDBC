3f91b8d80e0dc1ccb77e458b472863e9269e5c80
weed97 <weed9935@gmail.com>
2024-06-17 15:38:31 +0900

    chap02-statement

--- chap02-statement-lecture-source/src/main/java/com/ohgiraffers/section01/statement/Application03.java
+++ chap02-statement-lecture-source/src/main/java/com/ohgiraffers/section01/statement/Application03.java
@@ -1,6 +1,5 @@
 package com.ohgiraffers.section01.statement;
 
-import javax.management.Query;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,7 +9,7 @@ import java.util.Scanner;
 import static com.ohgiraffers.common.JDBCTemplate.close;
 import static com.ohgiraffers.common.JDBCTemplate.getConnection;
 
-public class Application02 {
+public class Application03 {
     public static void main(String[] args) {
 
         // 1. Connection 객체 생성
@@ -27,13 +26,14 @@ public class Application02 {
             // 4. 연결객체의 createStatement()를 이용한 Statement 객체 생성
             stmt = con.createStatement();
 
-            String empID = "200";
-            String query = "select emp_id,emp_name from employee where emp_id = '" + empID + "'";
-
+            Scanner sc = new Scanner(System.in);
+            System.out.println("조회하려는 사번을 입력하세요 : ");
+            String empId = sc.nextLine();
+            String query = "select emp_id,emp_name from employee where emp_id = '" + empId + "'";
             // 5. executeQuery()로 쿼리문을 실행하고 결과를 ResultSet 반환 받기
             rset = stmt.executeQuery(query);
 
-// 6. 쿼리문의 결과를 컬럼 이름음ㄹ 이용해서 사용
+            // 6. 쿼리문의 결과를 컬럼 이름음ㄹ 이용해서 사용
             if(rset.next()) {
                 System.out.println(rset.getString("EMP_ID") + ',' + rset.getString("EMP_NAME"));
             }
@@ -41,7 +41,7 @@ public class Application02 {
             throw new RuntimeException(e);
         } finally {
 
-
+            // 7. 사용한 자원 반납
             close(rset);
             close(stmt);
             close(con);
